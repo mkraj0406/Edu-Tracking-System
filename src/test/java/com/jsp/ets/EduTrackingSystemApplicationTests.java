28aa163fcecba3784733e60d7b5e16a4bde8fbba
Mayank Kumar <mkraj0406@gmail.com>
2024-08-24 11:23:56 +0530

    project initiated

---
+++ src/test/java/com/jsp/ets/EduTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.ets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
