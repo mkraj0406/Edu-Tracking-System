0e01ef7be18962244493c3cab450f4edd8a15bf0
Mayank Kumar <mkraj0406@gmail.com>
2024-08-26 11:28:07 +0530

    Entity Structured Defined and Custom Id  Generated

---
+++ src/main/java/com/jsp/ets/user/User.java
@@ -0,0 +1,46 @@
+package com.jsp.ets.user;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.jsp.ets.config.GenerateSequenceId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+	
+	
+	@Column(name = "userId")
+	@jakarta.persistence.Id
+	@GenerateSequenceId
+	private String userId;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "role")
+	private String role;
+
+	@Column(name = "created_date")
+	private LocalDateTime created_date;
+
+	@Column(name = "modified_date")
+	private LocalDateTime modified_date;
+
+}
