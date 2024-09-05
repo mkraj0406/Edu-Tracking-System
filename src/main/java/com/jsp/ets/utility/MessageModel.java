a9ef18041d9d3b59e5e1ab80b273d9121865ced0
Mayank Kumar <mkraj0406@gmail.com>
2024-09-05 12:15:27 +0530

    Mail sender and Message model class has been created to set the mail attrubutes

---
+++ src/main/java/com/jsp/ets/utility/MessageModel.java
@@ -0,0 +1,15 @@
+package com.jsp.ets.utility;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class MessageModel {
+    private String to;
+    private Date sendDate;
+    private  String subject;
+    private  String text;
+}
