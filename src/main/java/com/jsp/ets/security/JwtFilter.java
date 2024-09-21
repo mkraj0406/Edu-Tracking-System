4ca74f8fb21e130c3cf86ab37baa92066073668c
Mayank Kumar <mkraj0406@gmail.com>
2024-09-21 10:39:44 +0530

    removed unwanted import statements

--- src/main/java/com/jsp/ets/security/JwtFilter.java
+++ src/main/java/com/jsp/ets/security/JwtFilter.java
@@ -1,19 +1,13 @@
 package com.jsp.ets.security;
 
 
-import com.jsp.ets.user.UserRole;
-import io.jsonwebtoken.Claims;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
